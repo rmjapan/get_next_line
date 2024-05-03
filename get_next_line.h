7c5ddddfe35912034475126862f7323a929149b0
rmjapan <rmjapan2000@gmail.com>
2024-05-03 16:04:59 +0900

    2024/05/03

--- get_next_line.h
+++ get_next_line.h
@@ -10,17 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef GET_NEXT_LINE_H
-#  define GET_NEXT_LINE_H
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 # include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-
-char *get_next_line(int fd);
+char	*get_next_line(int fd);
 size_t	ft_strlen(const char *str);
 char	*ft_strjoin(char const *s1, char const *s2);
-char *ft_strchr(const char *s, int c);
+char	*ft_strchr(const char *s, int c);
 
-# endif
+#endif
